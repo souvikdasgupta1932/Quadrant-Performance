c072372d55219939a0f589b7b183e0a948bebba0
souvikdasgupta1932 <souvikdasgupta81@rediffmail.com>
2022-07-15 17:05:09 +0530

    Performance script

---
+++ script.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+	stages : [
+	{ duration: '15s', target: 100 },
+	{ duration: '30s', target: 100 },
+	{ duration: '15s', target: 0 },
+	],
+};
+	
+
+export default function () {
+  let res = http.get('https://platform.quadrant.io');
+  check(res, { 'status was 200': r => r.status == 200 });
+  sleep(1);
+}
